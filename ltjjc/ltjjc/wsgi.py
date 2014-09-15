debd1cb4da050d23cc6a7bf865c7b781593c422c
Elliott OHara <elliottohara@Elliotts-MacBook-Pro.local>
2014-09-15 18:10:13 -0500

    t

--- ltjjc/ltjjc/wsgi.py
+++ ltjjc/ltjjc/wsgi.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 import sys
+import ltjjc.settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ltjjc.settings")
 
 sys.path.append('~/ltccj.com/ltjjc/')
 sys.path.append('~/ltccj.com/')
-print("WTF OVER!")
+
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
